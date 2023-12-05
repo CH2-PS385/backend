45a203a0bdf798cbe9eb7f0883c2bb5af8f7be85
Afif Nur Fauzi <58578965+alscheift@users.noreply.github.com>
2023-12-06 05:44:09 +0700

    Api v1 (#1)
    * Add dataset and convert tensorflow function
    
    * add 1 api for testing
    
    * Rename folder
    
    * Split ml and data to another module
    
    * install body-parser
    npm i body-parser
    
    * testjson parse
    
    * 2 get API
    - getfoodbyid
    - getallfood
    
    * Add 2 api (allergen)
    - /v1/getallergenbyid
    - /v1/getallallergen
    
    * add recommend API
    
    * add modified name to food

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import * as ml from './ml/main.js';
+import * as data from './ml/data.js';
 import 'dotenv/config'
+import bodyParser from 'body-parser';
+import _ from 'lodash';
+
 
 const PORT = parseInt(process.env.PORT) || 8080;
 const app = express()
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send(["Hello", "World!"]);
@@ -15,6 +20,54 @@ app.get('/v0/test', async (req, res) => {
     res.send(foodsId);
 });
 
+
+app.post('/v0/testjson', async (req, res) => {
+    const json = req.body;
+    const test = {status:"test",json:json};
+    res.send(test);
+});
+
+app.get('/v1/getfoodbyid', async (req, res) => {
+    const id =  null ?? req.query.id;
+    if (!id) return res.status(400).send({success: false, message:'Missing parameter: id'});
+    const food = {...data.getFoodbyId(id)[0]};
+    if(_.isEmpty(food)) return res.status(404).send({success: false, message:'Food not found'});
+    return res.status(200).send({success: true, data: food});
+});
+
+app.get('/v1/getallfood', async(req, res) => {
+    return res.status(200).send({success: true, data: data.getAllFood()})
+});
+
+app.get('/v1/getallergenbyid', async(req, res) => {
+    const id =  null ?? req.query.id;
+    if (!id) return res.status(400).send({success: false, message:'Missing parameter: id'});
+    const allergen = {...data.getAllergenbyId(id)[0]};
+    if(_.isEmpty(allergen)) return res.status(404).send({success: false, message:'Allergen not found'});
+    return res.status(200).send({success: true, data: allergen});
+});
+
+app.post('/v1/recommend', async (req, res) => {
+    const calories = null ?? req.body.calories;
+    let allergies = null ?? req.body.allergies;
+    
+    
+    if (!calories && !allergies) return res.status(400).send({success: false, message:'Missing parameter: calories, allergies[]'});
+    if (!calories) return res.status(400).send({success: false, message:'Missing parameter: calories'});
+    if (!allergies) return res.status(400).send({success: false, message:'Missing parameter: allergies'});
+
+    allergies = allergies.map((val) => _.isInteger(val) ? data.getAllergenbyId(val)[0]?.allergen_name : val);
+    const invalidAllergies = allergies.findIndex((val) =>  _.isNull(val) || _.isUndefined(val));
+    if(invalidAllergies >= 0) return res.status(400).send({success: false, message:`Invalid Allergen: one or more allergen by id is not valid`});
+
+    const food_recommendation = await ml.recommendation(calories, allergies);
+     console.log(food_recommendation);
+    return res.status(200).send({success: true, data: food_recommendation.slice(0,4).map((val) =>({...data.getFoodbyId(val)[0]}))});
+});
+
+app.get('/v1/getallallergen', async(req, res) => {
+    return res.status(200).send({success: true, data: data.getAllAllergen()})
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
--- ml/data.js
+++ ml/data.js
@@ -1,14 +1,34 @@
 import 'dotenv/config';
 import fs from 'fs';
 import csvParser from 'csv-parser';
+import _ from 'lodash';
+
 
 const foodData_path = './ml/dataset_final.csv';
 
 const foods = [];
+const allergens = [];
 
-fs.createReadStream(foodData_path)
+await fs.createReadStream(foodData_path)
     .pipe(csvParser())
-    .on('data', (data) => foods.push(data))
+    .on('headers', (headers) => {
+        headers
+        .slice(10)
+        .map((val, idx) => allergens.push( 
+            {
+                id: idx,
+                allergen_name: val,
+                allergen_name_clean: _.startCase(val.replace('_', ' '))
+            }
+        ));
+    })
+    .on('data', (data) => {
+        // Clean the name 
+        let cleanName = data.nama_makanan.split(',')[0];
+        cleanName = _.startCase(cleanName);
+        data = {...data, nama_makanan_clean:cleanName}
+        foods.push(data);
+    })
     .on('end', () => {
     });
 
@@ -16,6 +36,18 @@ export const getFoodbyId = function (id) {
     return foods.filter((val) => val.food_id == id);
 }
 
+export const getAllFood = function () {
+    return foods;
+}
+
+export const getAllergenbyId = function (id) {
+    return allergens.filter((val) => id == val.id || id == val.allergen_name)
+}
+
+export const getAllAllergen = function () {
+    return allergens;
+}
+// == ML  ==
 // Get the kalori value from the food_id
 export const getCalories = function (id) {
     let calories = parseInt(foods[id - 1].kalori)
