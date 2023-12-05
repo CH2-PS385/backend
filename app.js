0b116f72f998bfec8a6f3fdfe84ccfdafc14f8f3
Afif Nur Fauzi <alscheift@gmail.com>
2023-12-05 17:36:06 +0700

    add 1 api for testing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import * as ml from './tensorflow/main.js';
+import 'dotenv/config'
 
 const PORT = parseInt(process.env.PORT) || 8080;
 const app = express()
@@ -6,6 +8,13 @@ const app = express()
 app.get('/', (req, res) => {
     res.send(["Hello", "World!"]);
 });
+
+app.get('/v0/test', async (req, res) => {
+    // const par = req.query;
+    const foodsId = await ml.recommendation(1800, ['daging_ayam', 'sayur']);
+    res.send(foodsId);
+});
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
