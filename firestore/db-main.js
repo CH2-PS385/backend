2e5128a30af4449941224761f0bcddbb0f1c125b
fadyanabila <88197471+fadyanabila@users.noreply.github.com>
2023-12-17 10:14:13 +0700

    Revised file DB

--- firestore/db-main.js
+++ firestore/db-main.js
@@ -1,19 +1,8 @@
-const admin = require('firebase-admin');
-
-// Initialize Firebase Admin SDK
-const serviceAccount = require('./firestore/ch2-ps385-7b79db54c90c.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://console.cloud.google.com/firestore/databases/-default-/data/panel/?project=ch2-ps385',
-});
-
-const db = admin.firestore();
+const db = require('./firebase'); 
 
 const usersCollection = db.collection('users');
 const plannerCollection = db.collection('planner');
 
-
-// Create a 'menu' document in the 'planner' collection
 const menu = plannerCollection.doc('menu');
 menu.set({
   callories: '',
@@ -24,7 +13,6 @@ menu.set({
   protein: '',
 });
 
-// Create a 'planner' document in the 'planner' collection
 const plan = plannerCollection.doc('plan');
 plan.set({
   callories: '',
@@ -33,15 +21,13 @@ plan.set({
   status: '',
 });
 
-// Create a 'water_tracking' document in the 'planner' collection
 const waterTracking = plannerCollection.doc('water_tracking');
 waterTracking.set({
   amount: [''],
-  date: admin.firestore.Timestamp.fromDate(new Date()),
+  date: db.Timestamp.fromDate(new Date()),
   total_amount: '',
 });
 
-// Create an 'account' document in the 'users' collection
 const account = usersCollection.doc('account');
 account.set({
   email: '',
@@ -49,7 +35,6 @@ account.set({
   username: '',
 });
 
-// Create a 'user_preferences' document in the 'users' collection
 const userPreferences = usersCollection.doc('user_preferences');
 userPreferences.set({
   age: '',
