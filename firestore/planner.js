69502bdfe032513080489876b0174e90dd6a3a6c
Afif Nur Fauzi <alscheift@gmail.com>
2023-12-18 21:10:05 +0700

    Fix set planner no document

--- firestore/planner.js
+++ firestore/planner.js
@@ -6,6 +6,7 @@ const planner = await db.collection('test_planners');
 export const set_planner = async function (email, dd, mm, yy, array) { // array of id
     
     try {
+        if(!(await planner.doc(email).get()).exists) planner.doc(email).set({});
         await planner.doc(email).update({
             [`${yy}${mm}${dd}`]: array
         });
